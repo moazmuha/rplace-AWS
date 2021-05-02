6780942d5d6cb9cff3354ea62b5d902f4826a26d
Muhammad Moaz <muh.moaz17@gmail.com>
2021-05-02 09:12:25 -0400

    dealing with table exists

--- createPointsTable.py
+++ createPointsTable.py
@@ -4,37 +4,41 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 # Create the DynamoDB table.
-table = dynamodb.create_table(
-    TableName='points',
-    KeySchema=[
-        {
-            'AttributeName': 'x',
-            'KeyType': 'HASH'
-        },
-        {
-            'AttributeName': 'y',
-            'KeyType': 'RANGE'
+try:
+    table = dynamodb.create_table(
+        TableName='points',
+        KeySchema=[
+            {
+                'AttributeName': 'x',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'y',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'x',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'y',
+                'AttributeType': 'S'
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
         }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'x',
-            'AttributeType': 'S'
-        },
-        {
-            'AttributeName': 'y',
-            'AttributeType': 'S'
-        }
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
+    )
+    # Wait until the table exists.
+    print("please wait...")
+    table.meta.client.get_waiter('table_exists').wait(TableName='points')
 
-# Wait until the table exists.
-print("please wait...")
-table.meta.client.get_waiter('table_exists').wait(TableName='points')
+    # Print out some data about the table.
+    print("table created succesfully")
 
-# Print out some data about the table.
-print("table created succesfully")
+except:
+    print("table already exists")
+    
