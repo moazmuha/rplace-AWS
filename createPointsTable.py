81f03597235faa5f696cf45da669229a2b55a746
Muhammad Moaz <66569506+moazmuha@users.noreply.github.com>
2021-05-02 08:41:26 -0400

    Update and rename create_table.py to createPointsTable.py

---
+++ createPointsTable.py
@@ -0,0 +1,39 @@
+import boto3
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='points',
+    KeySchema=[
+        {
+            'AttributeName': 'x',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'y',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'x',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'y',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='points')
+
+# Print out some data about the table.
+print("table created succesfully")
