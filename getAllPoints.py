ede582eb008c7406b0abf2ede2ac432640cdf1ab
Muhammad Moaz <muh.moaz17@gmail.com>
2021-05-04 20:13:25 -0400

    adding comments to scripts

--- getAllPoints.py
+++ getAllPoints.py
@@ -4,11 +4,15 @@ import boto3, sys
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 
+# Get the points table
 table = dynamodb.Table('points')
+
+#Grabb all the points in the table 
 try:
     create_time = table.creation_date_time
     response = table.scan()
     items = response['Items']
+    # return string of all points
     return_str = ''
     for item in items:
         return_str += str(item['x']) + "," + str(item["y"]) + "," + str(item['r']) + "," + str(item["g"]) + "," + str(item['b']) + ","
--- getRecentPoints.py
+++ getRecentPoints.py
@@ -5,20 +5,25 @@ from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('points')
+
 try:
     create_time = table.creation_date_time
+    # get current time
     now = datetime.now()
-    last_update = now - timedelta(seconds=10)
+    # time 6 seconds from now
+    last_update = now - timedelta(seconds=6)
 
     now = now.strftime('%FT%T+13:00')
     last_update = last_update.strftime('%FT%T+13:00')
 
+    # query points table for points added within the last 6 seconds
     fe = Key('timestamp').between(last_update,now);
     response = table.scan(
                     FilterExpression=fe
     )
 
     items = response['Items']
+    # return string of all points
     return_str = ''
     for item in items:
         return_str += str(item['x']) + "," + str(item["y"]) + "," + str(item['r']) + "," + str(item["g"]) + "," + str(item['b']) + ","
