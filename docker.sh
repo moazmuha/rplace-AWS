58be3d07e3bbceb6f2ca508db922cf63f1078a09
Muhammad Huzaifa <huzaifam2015@gmail.com>
2021-05-05 10:16:11 -0400

    allow client to reach server on port 80 http

--- docker.sh
+++ docker.sh
@@ -10,5 +10,5 @@ sudo service docker start
 #give ec2-user permissino to run docker without sudo
 sudo usermod -a -G docker ec2-user
 
-#run docker container
-docker run -p 8080:8080 -p 8081:8081 moazmuha/rplace
+#run docker container(ensure security group is configured to allow this traffic)
+docker run -p 80:8080 -p 8081:8081 moazmuha/rplace
