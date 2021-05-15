e43edc43522256b598f36fab62fad22d412f48ba
Muhammad Moaz <muh.moaz17@gmail.com>
2021-05-14 22:13:55 -0400

    fixing server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exec = require('child_process').exec;
 exec('./run.sh');
 
 //keep track of points that have been updated
-
+var updatedBoard = {};
 var dim = 1000; // note: you may change dimensions for more pixels
 var board= new Array(dim);
 for(var x=0;x<dim;x++){
