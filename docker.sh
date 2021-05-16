d3e628374a5ec8977b0443413fcaa389f26974b8
Muhammad Huzaifa <huzaifam2015@gmail.com>
2021-05-16 12:21:07 -0400

    docker.sh change

--- docker.sh
+++ docker.sh
@@ -2,15 +2,20 @@
 
 #bootstrap script for AWS instances
 #install docker(for AWS ubunutu AMI)
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo su
+apt update
+Y | apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt install docker-ce docker-ce-cli containerd.io
+Y | add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt install docker-ce docker-ce-cli containerd.io
 
 #check that docker was installed
-sudo systemctl status docker
+systemctl status docker
 
 #make sure credentials are set
 #run docker container(ensure security group is configured to allow this traffic)
-sudo docker run -p 80:8080 -p 8081:8081 moazmuha/rplace
+docker run -p 80:8080 -p 8081:8081 huzaifam/rplace
+
+#bootstrap script if created AMI
+#!/bin/bash
+sudo docker run -p 80:8080 -p 8081:8081 huzaifam/rplace
